06be3d9e517751eb6579b1de1493dbfe02efcdd1
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-07 15:38:10 -0300

    Initial configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/generic/_box-sizing.css';
+import './styles/generic/_resets.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
