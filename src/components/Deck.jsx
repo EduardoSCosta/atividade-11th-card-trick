d666e5916e0feb248295956c5495828d95c980c5
EduardoSCosta <edu_costaaraujo@hotmail.com>
2022-03-08 12:31:00 -0300

    create CardRows component

---
+++ src/components/Deck.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import CardsRow from './CardsRow';
+import twentyOneCardsDeck from '../utils/createTwentyOneCardsDeck';
+import filterDeckRows from '../utils/filterDeckRows';
+
+function Deck() {
+
+  const [deck, setDeck] = useState(twentyOneCardsDeck());
+  const [firstRow, setFirstRow] = useState([]);
+  const [secondRow, setSecondRow] = useState([]);
+  const [thirdRow, setThirdRow] = useState([]);
+
+  useEffect(()=> {
+    setFirstRow(filterDeckRows(deck, 0));
+    setSecondRow(filterDeckRows(deck, 1));
+    setThirdRow(filterDeckRows(deck, 2));
+  }, [deck])
+
+  return (
+    <div className='deck'>
+      <CardsRow cards={firstRow}/>
+      <CardsRow cards={secondRow}/>
+      <CardsRow cards={thirdRow}/>
+    </div>
+  );
+}
+
+export default Deck;
